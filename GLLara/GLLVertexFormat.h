7d23a2ee532d35eb756fe872cd7809c7f7408abf
cochrane <torsten@ferroequinologist.de>
2012-09-03 01:22:04 +0200

    Definition of vertex attrib indices.
    The one thing OpenGL still lets us assign! Which means special care is needed. Such as an enum.

---
+++ GLLara/GLLVertexFormat.h
@@ -0,0 +1,23 @@
+//
+//  GLLVertexFormat.h
+//  GLLara
+//
+//  Created by Torsten Kammer on 01.09.12.
+//  Copyright (c) 2012 Torsten Kammer. All rights reserved.
+//
+
+#ifndef GLLara_GLLVertexFormat_h
+#define GLLara_GLLVertexFormat_h
+
+enum GLLVertexAttrib
+{
+	GLLVertexAttribPosition,
+	GLLVertexAttribNormal,
+	GLLVertexAttribColor,
+	GLLVertexAttribBoneIndices,
+	GLLVertexAttribBoneWeights,
+	GLLVertexAttribTexCoord0,
+	GLLVertexAttribTangent0
+};
+
+#endif
