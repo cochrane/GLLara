56347228451bdb1b829ddb78220c564eff28d799
cochrane <torsten@ferroequinologist.de>
2012-09-04 10:37:26 +0200

    List of all shaders, to encapsulate what vertex shader goes with what fragment shader, and what uniform values to set (and in what order, when coming from the generic format).
    Is this the simplest class ever or what? Sure, I could have just used the NSDictionary directly, but that is so ugly.

---
+++ GLLara/GLLShaderList.h
@@ -0,0 +1,22 @@
+//
+//  GLLShaderList.h
+//  GLLara
+//
+//  Created by Torsten Kammer on 04.09.12.
+//  Copyright (c) 2012 Torsten Kammer. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GLLShaderList : NSObject
+
++ (id)defaultShaderList;
+
+- (id)initWithPlist:(NSDictionary *)propertyList;
+
+@property (nonatomic, copy, readonly) NSArray *shaderNames;
+- (NSString *)vertexShaderForName:(NSString *)shaderName;
+- (NSString *)fragmentShaderForName:(NSString *)shaderName;
+- (NSArray *)renderParameterNamesForName:(NSString *)shaderName;
+
+@end
