3d18963db84fd2aed5e8816ee4d8895af3fd8a1a
cochrane <torsten@ferroequinologist.de>
2012-09-04 11:00:44 +0200

    That is necessary, too.

---
+++ GLLara/GLLUniformBlockBindings.h
@@ -0,0 +1,19 @@
+//
+//  GLLUniformBlockBindings.h
+//  GLLara
+//
+//  Created by Torsten Kammer on 03.09.12.
+//  Copyright (c) 2012 Torsten Kammer. All rights reserved.
+//
+
+#ifndef GLLara_GLLUniformBlockBindings_h
+#define GLLara_GLLUniformBlockBindings_h
+
+enum GLLUniformBlockBindings
+{
+	GLLUniformBlockBindingTransforms,
+	GLLUniformBlockBindingLights,
+	GLLUniformBlockBindingRenderParameters
+};
+
+#endif
