5a8a26123a4296fe45a550743818e745a6531303
cochrane <torsten@ferroequinologist.de>
2012-09-07 01:21:20 +0200

    Added a new shader.
    I'm getting the hang of this now. DiffuseLightmapBumpSpecular was fairly easy with DiffuseLightmapBump and DiffuseLightmapBumpSpecular3 for comparison.

---
+++ GLLara/Shaders/DiffuseLightmapBumpSpecular.fs
@@ -0,0 +1,70 @@
+/*
+ * Same as DiffuseBump, except for the lightmap, which is multiplied into the color at every step. Come to think of it, I could multiply it at the end, too, but either way, I'm not 100% certain why this texture exists.
+ */
+#version 150
+
+in vec4 outColor;
+in vec2 outTexCoord;
+in vec3 normalWorld;
+in vec3 positionWorld;
+in mat3 tangentToWorld;
+
+out vec4 screenColor;
+
+uniform sampler2D diffuseTexture;
+uniform sampler2D lightmapTexture;
+uniform sampler2D bumpTexture;
+uniform sampler2D specularTexture;
+
+uniform vec3 cameraPosition;
+
+struct Light {
+	vec4 color;
+	vec3 direction;
+	float intensity;
+	float shadowDepth;
+};
+
+layout(std140) uniform LightData {
+	vec3 cameraPosition;
+	Light lights[3];
+} lightData;
+
+uniform RenderParameters {
+	float bumpSpecularGloss;
+	float bumpSpecularAmount;
+} parameters;
+
+void main()
+{
+	vec4 diffuseColor = texture(diffuseTexture, outTexCoord) * outColor;
+	vec4 normalMap = texture(bumpTexture, outTexCoord);
+	vec4 lightmapColor = texture(lightmapTexture, outTexCoord);
+	vec4 specularColor = texture(specularTexture, outTexCoord);
+	
+	vec3 normalFromMap = vec3(normalMap.rg * 2 - 1, normalMap.b);
+	vec3 normal = normalize(tangentToWorld * normalFromMap);
+	
+	vec3 cameraDirection = normalize(positionWorld - lightData.cameraPosition);
+	
+	vec4 color = vec4(0);
+	for (int i = 0; i < 3; i++)
+	{
+		// Calculate diffuse factor
+		float diffuseFactor = clamp(dot(normal, -lightData.lights[i].direction), 0, 1);
+		
+		// Calculate specular factor
+		vec3 refLightDir = -reflect(lightData.lights[i].direction, normal);
+		float specularFactor = clamp(dot(cameraDirection, refLightDir), 0, 1);
+		float specularShading = diffuseFactor * pow(specularFactor, parameters.bumpSpecularGloss) * parameters.bumpSpecularAmount;
+		
+		// Add specular color to the color
+		// Include lightmap color, too.
+		vec4 lightenedColor = diffuseColor + specularColor * specularShading * lightData.lights[i].intensity;
+		color += lightData.lights[i].color * diffuseFactor * lightenedColor * lightmapColor;
+	}
+	
+	color.a = diffuseColor.a;
+	
+	screenColor = color;
+}
