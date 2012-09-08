22e1e4c517d94a7d8927e4ae4ef43a603acffc31
cochrane <torsten@ferroequinologist.de>
2012-09-08 23:10:10 +0200

    Added Metallic shader, fixed issue with MetallicBump3.
    Now it should all look as it was meant to. Well, not really. Better, if I have anything to say about it.

--- GLLara/Shaders/Metallic.fs
+++ GLLara/Shaders/Metallic.fs
@@ -1,5 +1,5 @@
 /*
- * This is essentially identical to DiffuseLightmapBump3, but the specular color is not always white; instead it is read from its own texture.
+ * Now this one's a weird shader. It uses bump maps and reflection, but does not add any specular highlights. I'll be damned if I know why. I use the normal maps for the diffuse component anyway.
  */
 #version 150
 
@@ -11,14 +11,8 @@ in mat3 tangentToWorld;
 out vec4 screenColor;
 
 uniform sampler2D diffuseTexture;
-uniform sampler2D lightmapTexture;
 uniform sampler2D bumpTexture;
-uniform sampler2D bump1Texture;
-uniform sampler2D bump2Texture;
-uniform sampler2D maskTexture;
 uniform sampler2D reflectionTexture;
-
-uniform sampler2D specularTexture;
 
 struct Light {
 	vec4 color;
@@ -33,9 +27,6 @@ layout(std140) uniform LightData {
 } lightData;
 
 uniform RenderParameters {
-	float bumpSpecularGloss;
-	float bumpSpecularAmount;
-	float bumpUVScale;
 	float reflectionAmount;
 } parameters;
 
@@ -52,18 +43,8 @@ void main()
 	vec4 diffuseColor = diffuseTexColor * outColor;
 	
 	vec4 normalMap = texture(bumpTexture, outTexCoord);
-	vec4 detailNormalMap1 = texture(bump1Texture, outTexCoord * parameters.bumpUVScale);
-	vec4 detailNormalMap2 = texture(bump2Texture, outTexCoord * parameters.bumpUVScale);
-	vec4 maskColor = texture(maskTexture, outTexCoord);
 	
-	vec4 lightmapColor = texture(lightmapTexture, outTexCoord);
-	vec4 specularColor = texture(specularTexture, outTexCoord);
-	
-	// Combine normal textures
-	vec3 normalColor = normalMap.rgb + (detailNormalMap1.rgb - 0.5) * maskColor.r + (detailNormalMap2.rgb - 0.5) * maskColor.g;
-	
-	// Derive actual normal
-	vec3 normalFromMap = vec3(normalMap.rg * 2 - 1, normalMap.b);
+	vec3 normalFromMap = normalMap.rgb * 2 - 1;
 	vec3 normal = normalize(tangentToWorld * normalFromMap);
 	
 	vec3 cameraDirection = normalize(lightData.cameraPosition - positionWorld);
@@ -71,20 +52,11 @@ void main()
 	vec4 color = vec4(0);
 	for (int i = 0; i < 3; i++)
 	{
-		// Calculate diffuse factor
 		float diffuseFactor = clamp(dot(normal, -lightData.lights[i].direction.xyz), 0, 1);
 		// Apply the shadow depth that is used instead of ambient lighting
 		diffuseFactor = mix(1, diffuseFactor, lightData.lights[i].shadowDepth);
 		
-		// Calculate specular factor
-		vec3 refLightDir = reflect(lightData.lights[i].direction.xyz, normal);
-		float specularFactor = clamp(dot(cameraDirection, refLightDir), 0, 1);
-		float specularShading = diffuseFactor * pow(specularFactor, parameters.bumpSpecularGloss) * parameters.bumpSpecularAmount;
-		
-		// Add specular color to the color
-		// Include lightmap color, too.
-		vec4 lightenedColor = diffuseColor + specularColor * specularShading * lightData.lights[i].intensity;
-		color += lightData.lights[i].color * diffuseFactor * lightenedColor * lightmapColor;
+		color += lightData.lights[i].color * diffuseFactor;
 	}
 	
 	// Apply reflection
