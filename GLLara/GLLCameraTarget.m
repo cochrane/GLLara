9a4169c0f9c4c9834db77bef050990cb0edc5827
cochrane <torsten@ferroequinologist.de>
2012-09-10 12:52:30 +0200

    Camera targets now work properly.
    I don't like when an object observes itself, but in Core Data, I am not sure that I can trust anything else (i.e. adding the observers in the public accessors).

--- GLLara/GLLCameraTarget.m
+++ GLLara/GLLCameraTarget.m
@@ -12,6 +12,15 @@
 #import "GLLItem.h"
 #import "simd_functions.h"
 
+static void *contextMarker = (void *) 0xdeadbeef;
+
+@interface GLLCameraTarget ()
+
+@property (nonatomic, assign, readwrite) vec_float4 position;
+- (void)_updatePosition;
+
+@end
+
 @implementation GLLCameraTarget
 
 + (NSSet *)keyPathsForValuesAffectingDisplayName
@@ -19,27 +28,61 @@
 	return [NSSet setWithObjects:@"name", @"bones", nil];
 }
 
-+ (NSSet *)keyPathsForValuesAffectingPosition
-{
-	return [NSSet setWithObject:@"bones.globalPosition"];
-}
+@synthesize position;
 
 @dynamic name;
 @dynamic bones;
 @dynamic cameras;
+
+- (void)awakeFromFetch
+{
+	[super awakeFromFetch];
+	[self addObserver:self forKeyPath:@"bones" options:NSKeyValueObservingOptionInitial | NSKeyValueObservingOptionNew | NSKeyValueObservingOptionOld context:contextMarker];
+}
+- (void)awakeFromInsert
+{
+	[super awakeFromInsert];
+	[self addObserver:self forKeyPath:@"bones" options:NSKeyValueObservingOptionInitial | NSKeyValueObservingOptionNew | NSKeyValueObservingOptionOld context:contextMarker];
+}
+- (void)awakeFromSnapshotEvents:(NSSnapshotEventType)flags
+{
+	[super awakeFromSnapshotEvents:flags];
+	[self addObserver:self forKeyPath:@"bones" options:NSKeyValueObservingOptionInitial | NSKeyValueObservingOptionNew | NSKeyValueObservingOptionOld context:contextMarker];
+}
+- (void)willTurnIntoFault
+{
+	[self removeObserver:self forKeyPath:@"bones"];
+}
+- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context
+{
+	if (context == contextMarker && [keyPath isEqual:@"bones"])
+	{
+		for (GLLBoneTransformation *transform in change[NSKeyValueChangeOldKey])
+			[transform removeObserver:self forKeyPath:@"relativeTransform"];
+		
+		for (GLLBoneTransformation *transform in change[NSKeyValueChangeNewKey])
+			[transform addObserver:self forKeyPath:@"relativeTransform" options: NSKeyValueObservingOptionInitial context:contextMarker];
+	}
+	else if (context == contextMarker && [keyPath isEqual:@"relativeTransform"])
+	{
+		[self _updatePosition];
+	}
+	else
+		[super observeValueForKeyPath:keyPath ofObject:object change:change context:context];
+}
 
 - (NSString *)displayName
 {
 	return [NSString stringWithFormat:NSLocalizedString(@"%@ — %@", @"camera target name format"), self.name, [[self.bones.anyObject item] displayName]];
 }
 
-- (vec_float4)position
+- (void)_updatePosition
 {
-	vec_float4 position;
+	vec_float4 newPosition;
 	for (GLLBoneTransformation *transform in self.bones)
-		position += transform.globalPosition;
+		newPosition += transform.globalPosition;
 	
-	return position / simd_splatf(self.bones.count);
+	self.position = newPosition / simd_splatf(self.bones.count);
 }
 
 @end
