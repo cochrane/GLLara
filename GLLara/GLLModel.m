0ed4b5eea50a51938ea16dbf27a8359a2248dfa7
cochrane <torsten@ferroequinologist.de>
2012-09-11 16:23:23 +0200

    Hacked in very bad support for Generic Item 2.
    This was what I could understand from the files I had. I don't like it, but it seems to work. Kind of.

--- GLLara/GLLModel.m
+++ GLLara/GLLModel.m
@@ -78,12 +78,56 @@ static NSCache *cachedModels;
 	
 	TRInDataStream *stream = [[TRInDataStream alloc] initWithData:data];
 	
+	BOOL isGenericItem2 = NO;
+	
 	NSUInteger header = [stream readUint32];
 	if (header == 323232)
 	{
-		if (error)
-			*error = [NSError errorWithDomain:@"GLLModel" code:1 userInfo:@{ NSLocalizedDescriptionKey : NSLocalizedString(@"New-style Generic Item models are not supported.", @"Found magic byte at start of model file"), NSLocalizedRecoverySuggestionErrorKey : NSLocalizedString(@"If there is a .mesh.ascii version, try opening that.", @"New-style binary generic item won't work.")}];
-		return nil;
+		/*
+		 * This is my idea of how to support the Generic Item 2 format. Note
+		 * that this is all reverse engineered from looking at files. I do not
+		 * know whether my variable names are correct, and I do not interpret
+		 * it in any way.
+		 */
+		NSLog(@"Warning: Using experimental, hackish, ugly Generic Item 2 support");
+		isGenericItem2 = YES;
+		
+		// First: Two uint16s. My guess: Major, then minor version.
+		// Always 1 and 12.
+		uint16_t possiblyMajorVersion = [stream readUint16];
+		if (possiblyMajorVersion != 0x0001)
+		{
+			if (error)
+				*error = [NSError errorWithDomain:@"GLLModel" code:10 userInfo:@{ NSLocalizedDescriptionKey : NSLocalizedString(@"New-style Generic Item has wrong major version.", @"Generic Item 2: Expected 0x0001 at offset 4"), NSLocalizedRecoverySuggestionErrorKey : NSLocalizedString(@"If there is a .mesh.ascii version, try opening that.", @"New-style binary generic item won't work.")}];
+			return nil;
+		}
+		uint16_t possiblyMinorVersion = [stream readUint16];
+		if (possiblyMinorVersion != 0x000C)
+		{
+			if (error)
+				*error = [NSError errorWithDomain:@"GLLModel" code:10 userInfo:@{ NSLocalizedDescriptionKey : NSLocalizedString(@"New-style Generic Item has wrong minor version.", @"Generic Item 2: Expected 0x000C at offset 6"), NSLocalizedRecoverySuggestionErrorKey : NSLocalizedString(@"If there is a .mesh.ascii version, try opening that.", @"New-style binary generic item won't work.")}];
+			return nil;
+		}
+		
+		// A string. In all files that I've seen it is XNAaraL.
+		NSString *toolAuthor = [stream readPascalString];
+		if (![toolAuthor isEqual:@"XNAaraL"])
+			NSLog(@"Unusual string at offset %lu: Expected XNAaraL, got %@", stream.position, toolAuthor);
+		
+		// A count of… thingies that appear after the next three strings. Skip that count times four bytes and you are ready to read bones.
+		NSUInteger countOfUnknownint32s = [stream readUint32];
+		
+		// Three strings. The third is often just a letter (e.g. X).
+		NSString *firstAuxilaryString = [stream readPascalString];
+		NSString *secondAuxilaryString = [stream readPascalString];
+		NSString *thirdAuxilaryString = [stream readPascalString];
+		NSLog(@"Auxilary strings: %@, %@, %@", firstAuxilaryString, secondAuxilaryString, thirdAuxilaryString);
+		
+		// The thingies from above. All the same value in the models I've seen so far, typically small integers (0 or 3). Not sure what count relates to; is not bone count, mesh count, bone count + mesh count or anything like that.
+		[stream skipBytes:4 * countOfUnknownint32s];
+		
+		// Read number of bones.
+		header = [stream readUint32];
 	}
 	
 	NSUInteger numBones = header;
@@ -97,6 +141,20 @@ static NSCache *cachedModels;
 	for (NSUInteger i = 0; i < numMeshes; i++)
 		[self _addMesh:[[GLLMesh alloc] initFromStream:stream partOfModel:self] toArray:meshes];
 	_meshes = meshes;
+	
+	if (isGenericItem2)
+	{
+		// A string; always $$XNAaraL$$
+		NSString *footerAuthor = [stream readPascalString];
+		if (![footerAuthor isEqual:@"$$XNAaraL$$"])
+			NSLog(@"Unusual string at offset %lu: Expected $$XNAaraL$$, got %@", stream.position, footerAuthor);
+
+		NSString *copyrightNotice = [stream readPascalString];
+		NSLog(@"Copyright notice: %@", copyrightNotice);
+		
+		NSString *creationToolText = [stream readPascalString];
+		NSLog(@"Creation tool text: %@", creationToolText);
+	}
 	
 	return self;
 }
