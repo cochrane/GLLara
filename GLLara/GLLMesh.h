26b1eaaee90e1fdce68aa8cb70fe6273a578f35c
cochrane <torsten@ferroequinologist.de>
2012-09-13 13:37:02 +0200

    Made drawing information writeable. This should only be used by subclasses during their initialization!

--- GLLara/GLLMesh.h
+++ GLLara/GLLMesh.h
@@ -84,9 +84,9 @@
 /*
  * Drawing information, gained through the model parameters. This information is not stored in the mesh file.
  */
-@property (nonatomic, copy, readonly) GLLShaderDescriptor *shader;
-@property (nonatomic, assign, readonly) BOOL usesAlphaBlending;
-@property (nonatomic, copy, readonly) NSDictionary *renderParameterValues;
+@property (nonatomic, copy) GLLShaderDescriptor *shader;
+@property (nonatomic, assign) BOOL usesAlphaBlending;
+@property (nonatomic, copy) NSDictionary *renderParameterValues;
 
 // To be used by subclasses. Calculates the tangents based on the texture coordinates, and fills them in the correct fields of the data, using the offsets and strides of the file
 - (void)calculateTangents:(NSMutableData *)vertexData;
