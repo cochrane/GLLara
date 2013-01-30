79214c2b56d5653e46ef49c72a67258a13308e47
cochrane <torsten@ferroequinologist.de>
2013-01-30 11:50:30 +0100

    Ensure that the C++ error is actually seen.
    Remember: It's the recovery suggestion that gets shown, not anything else.

--- GLLara/GLLModelObj.mm
+++ GLLara/GLLModelObj.mm
@@ -43,7 +43,7 @@
 		if (error)
 			*error = [NSError errorWithDomain:@"GLLModelObj" code:1 userInfo:@{
 				   NSLocalizedDescriptionKey : NSLocalizedString(@"There was an error loading the file.", @"couldn't load obj file"),
-			NSLocalizedFailureReasonErrorKey : [NSString stringWithFormat:NSLocalizedString(@"Underlying error: %s", @"C++ threw exception"), e.what()]
+			NSLocalizedRecoverySuggestionErrorKey : [NSString stringWithFormat:NSLocalizedString(@"Underlying error: %s", @"C++ threw exception"), e.what()]
 					  }];
 		return nil;
 	}
