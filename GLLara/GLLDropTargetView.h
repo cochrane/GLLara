7cfa494bb9c45ed2be0f4e3fb899c25aa84ea579
cochrane <torsten@ferroequinologist.de>
2017-06-22 21:21:24 +0200

    Add models to document by dropping on "No selection" view
    Previously adding models to a new document always involved clicking
    some button or menu item and then selecting the model directly. That
    was annoying, especially when trying out a lot of different new models
    quickly to see what does and does not work about them. To fix this, it
    is now possible to add models by simply dragging them on the view „no
    selection“ view directly.
    
    Further steps will include adding some text that tells people that this
    is possible, and adding files by dragging them in the objects list on
    the left.

---
+++ GLLara/GLLDropTargetView.h
@@ -0,0 +1,15 @@
+//
+//  GLLDropTargetView.h
+//  GLLara
+//
+//  Created by Torsten Kammer on 22.06.17.
+//  Copyright © 2017 Torsten Kammer. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@class GLLDocument;
+
+@interface GLLDropTargetView : NSView
+
+@end
---
+++ GLLara/GLLDropTargetView.m
@@ -0,0 +1,69 @@
+//
+//  GLLDropTargetView.m
+//  GLLara
+//
+//  Created by Torsten Kammer on 22.06.17.
+//  Copyright © 2017 Torsten Kammer. All rights reserved.
+//
+
+#import "GLLDropTargetView.h"
+
+#import "GLLDocument.h"
+
+@implementation GLLDropTargetView
+
+- (void)awakeFromNib {
+    [self registerForDraggedTypes:@[ (__bridge NSString*) kUTTypeFileURL ]];
+}
+
+- (NSDragOperation)draggingEntered:(id<NSDraggingInfo>)sender {
+    NSWindowController *windowController = self.window.windowController;
+    GLLDocument *document = (GLLDocument *) windowController.document;
+    if (!document) {
+        return NSDragOperationNone;
+    }
+    
+    NSPasteboard *pasteboard = sender.draggingPasteboard;
+    for (NSPasteboardItem *item in pasteboard.pasteboardItems) {
+        // Wait, does Apple really not have a method to get multiple URLs from
+        // the pasteboard? I'm not complaining, just surprised.
+        NSString *urlAsString = [item stringForType:(__bridge NSString*) kUTTypeFileURL];
+        NSURL *url = [NSURL URLWithString:urlAsString];
+        NSString *filename = [url filePathURL].lastPathComponent.lowercaseString;
+        
+        // Note: Not using dedicated methods because they can't deal with the
+        // double extension that XNALara uses.
+        if (![filename hasSuffix:@".mesh"] && ![filename hasSuffix:@".mesh.ascii"] && ![filename hasSuffix:@".xps"]) {
+            return NSDragOperationNone;
+        }
+    }
+    
+    return NSDragOperationCopy;
+}
+
+- (BOOL)performDragOperation:(id<NSDraggingInfo>)sender {
+    NSWindowController *windowController = self.window.windowController;
+    GLLDocument *document = (GLLDocument *) windowController.document;
+    if (!document) {
+        return NO;
+    }
+    
+    NSPasteboard *pasteboard = sender.draggingPasteboard;
+    for (NSPasteboardItem *item in pasteboard.pasteboardItems) {
+        // Wait, does Apple really not have a method to get multiple URLs from
+        // the pasteboard? I'm not complaining, just surprised.
+        NSString *urlAsString = [item stringForType:(__bridge NSString*) kUTTypeFileURL];
+        NSURL *url = [NSURL URLWithString:urlAsString];
+        
+        NSError *error = nil;
+        [document addModelAtURL:url error:&error];
+        if (error) {
+            [self presentError:error];
+            return NO;
+        }
+    }
+    
+    return YES;
+}
+
+@end
