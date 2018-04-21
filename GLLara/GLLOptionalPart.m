c9f5bf8123605cc31ceccc62a97cd5d84e50830a
cochrane <torsten@ferroequinologist.de>
2018-04-21 08:29:56 +0200

    Properly handle optional parts that are all invisible at the start
    Because in that case the meshes starting with „-„ are not alternates
    that need special handling, they’re just meshes.

--- GLLara/GLLOptionalPart.m
+++ GLLara/GLLOptionalPart.m
@@ -12,10 +12,12 @@
 #import "GLLItemMesh.h"
 
 @interface GLLOptionalPart() {
-    BOOL updating;
+    // Whether all meshes are invisible at the start. If yes, it's simply an
+    // inactive part and the meshes get treated normally (visibility = our
+    // visibility). Otherwise meshes starting with a "-" are alternate meshes,
+    // to be shown when the part is turned off.
+    BOOL initiallyAllInvisible;
 }
-
-- (BOOL)meshIsRelevant:(GLLItemMesh *)mesh;
 
 @end
 
@@ -30,10 +32,21 @@
     _item = item;
     _name = name;
     
+    BOOL haveVisibles = NO;
+    BOOL haveInvisibles = NO;
     for (GLLItemMesh *mesh in item.meshes) {
-        if ([self meshIsRelevant:mesh]) {
+        if ([mesh.displayName hasPrefix:[NSString stringWithFormat:@"-%@", self.name]]) {
+            // Invisible for this item
             [mesh addObserver:self forKeyPath:@"isVisible" options:0 context:NULL];
+            haveInvisibles = haveInvisibles || YES;
+        } else if ([mesh.displayName hasPrefix:[NSString stringWithFormat:@"+%@", self.name]]) {
+            // Visible for this item
+            [mesh addObserver:self forKeyPath:@"isVisible" options:0 context:NULL];
+            haveVisibles = haveVisibles || YES;
         }
+    }
+    if (haveInvisibles && !haveVisibles) {
+        initiallyAllInvisible = YES;
     }
     
     return self;
@@ -47,9 +60,6 @@
 
 - (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary<NSKeyValueChangeKey,id> *)change context:(void *)context {
     if ([keyPath isEqual:@"isVisible"]) {
-        if (updating)
-            return;
-        
         [self willChangeValueForKey:@"visible"];
         [self didChangeValueForKey:@"visible"];
     } else {
@@ -62,9 +72,15 @@
     BOOL foundInactive = NO;
     for (GLLItemMesh *mesh in self.item.meshes) {
         if ([mesh.displayName hasPrefix:[NSString stringWithFormat:@"-%@", self.name]]) {
-            // Supposed to be invisible for this item
-            foundActive = foundActive || !mesh.isVisible;
-            foundInactive = foundInactive || mesh.isVisible;
+            if (initiallyAllInvisible) {
+                // Supposed to be visible for this item
+                foundActive = foundActive || mesh.isVisible;
+                foundInactive = foundInactive || !mesh.isVisible;
+            } else {
+                // Supposed to be invisible for this item
+                foundActive = foundActive || !mesh.isVisible;
+                foundInactive = foundInactive || mesh.isVisible;
+            }
         } else if ([mesh.displayName hasPrefix:[NSString stringWithFormat:@"+%@", self.name]]) {
             // Supposed to be visible for this item
             foundActive = foundActive || mesh.isVisible;
@@ -86,24 +102,19 @@
     [self willChangeValueForKey:@"visible"];
     for (GLLItemMesh *mesh in self.item.meshes) {
         if ([mesh.displayName hasPrefix:[NSString stringWithFormat:@"-%@", self.name]]) {
-            // Invisible for this item
-            mesh.isVisible = ![visible boolValue];
+            if (initiallyAllInvisible) {
+                // Visible for this item
+                mesh.isVisible = [visible boolValue];
+            } else {
+                // Invisible for this item
+                mesh.isVisible = ![visible boolValue];
+            }
         } else if ([mesh.displayName hasPrefix:[NSString stringWithFormat:@"+%@", self.name]]) {
             // Visible for this item
             mesh.isVisible = [visible boolValue];
         }
     }
     [self didChangeValueForKey:@"visible"];
-}
-
-- (BOOL)meshIsRelevant:(GLLItemMesh *)mesh {
-    if ([mesh.displayName hasPrefix:[NSString stringWithFormat:@"-%@", self.name]]) {
-        return YES;
-    }
-    if ([mesh.displayName hasPrefix:[NSString stringWithFormat:@"+%@", self.name]]) {
-        return YES;
-    }
-    return NO;
 }
 
 @end
